910381f7805efc7016c52d8401fcebd6edfa560d
Oleh Lutsenko <pompolutz@gmail.com>
2024-04-25 08:45:51 +0000

    add usePortal hook

--- apps/frontend/src/atoms/FightersInfoList.jsx
+++ apps/frontend/src/atoms/FightersInfoList.jsx
@@ -4,87 +4,83 @@ import { useSpring, animated as a } from "@react-spring/web";
 import { useDeckBuilderState } from "../pages/DeckCreator";
 
 function useClickAway() {
-    const [clickedAway, setClickedAway] = useState(false);
+  const [clickedAway, setClickedAway] = useState(false);
 
-    useEffect(() => {
-        const handleClick = (e) => {
-            let el = document.elementFromPoint(e.clientX, e.clientY);
-            if (el && el.nodeName !== "IMG") {
-                setClickedAway(true);
-            }
-        };
+  useEffect(() => {
+    const handleClick = (e) => {
+      let el = document.elementFromPoint(e.clientX, e.clientY);
+      if (el && el.nodeName !== "IMG") {
+        setClickedAway(true);
+      }
+    };
 
-        const handleKeyUp = (e) => {
-            if (e.key === "Escape") {
-                setClickedAway(true);
-            }
-        };
+    const handleKeyUp = (e) => {
+      if (e.key === "Escape") {
+        setClickedAway(true);
+      }
+    };
 
-        document.addEventListener("click", handleClick);
-        document.addEventListener("keyup", handleKeyUp);
+    document.addEventListener("click", handleClick);
+    document.addEventListener("keyup", handleKeyUp);
 
-        return () => {
-            document.removeEventListener("click", handleClick);
-            document.removeEventListener("keyup", handleKeyUp);
-        };
-    }, []);
+    return () => {
+      document.removeEventListener("click", handleClick);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
 
-    return clickedAway;
+  return clickedAway;
 }
 
 export default function FightersInfoList({ onClose }) {
-    const { faction } = useDeckBuilderState();
-    const clickedAway = useClickAway();
+  const { faction } = useDeckBuilderState();
+  const clickedAway = useClickAway();
 
-    useEffect(() => {
-        if (clickedAway) {
-            onClose();
-        }
-    }, [clickedAway, onClose]);
+  useEffect(() => {
+    if (clickedAway) {
+      onClose();
+    }
+  }, [clickedAway, onClose]);
 
-    return (
-        <div className="flex-1 relative">
-            <div className="absolute inset-0 overflow-y-auto p-4 lg:p-12">
-                {factionMembers[faction.name].map((fighter, index) => (
-                    <FighterCard
-                        key={fighter}
-                        faction={faction.name}
-                        index={index + 1}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex-1 relative">
+      <div className="absolute inset-0 overflow-y-auto p-4 lg:p-12">
+        {factionMembers[faction.name].map((fighter, index) => (
+          <FighterCard key={fighter} faction={faction.name} index={index + 1} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 function FighterCard({ faction, index }) {
-    const [flipped, set] = useState(false);
-    const { transform, opacity } = useSpring({
-        opacity: flipped ? 1 : 0,
-        transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
-        config: { mass: 5, tension: 500, friction: 80 },
-    });
-    return (
-        <div
-            className="grid px-4 sm:px-0 mb-4 lg:w-1/3 lg:mx-auto cursor-pointer"
-            onClick={() => set((state) => !state)}
-        >
-            <a.img
-                className="w-full rounded-sm sm:w-3/4 row-start-1 col-start-1 sm:mx-auto cursor-pointer hover:shadow-lg"
-                src={`/assets/fighters/${faction}/${faction}-${index}.png`}
-                style={{
-                    opacity: opacity.to((o) => 1 - o),
-                    transform,
-                }}
-            />
-            <a.img
-                className="w-full rounded-sm sm:w-3/4 row-start-1 col-start-1 sm:mx-auto cursor-pointer hover:shadow-lg"
-                src={`/assets/fighters/${faction}/${faction}-${index}-inspired.png`}
-                style={{
-                    opacity,
-                    transform: transform.to((t) => `${t} rotateY(180deg)`),
-                }}
-            />
-        </div>
-    );
+  const [flipped, set] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+  return (
+    <div
+      className="grid px-4 sm:px-0 mb-4 lg:w-1/3 lg:mx-auto cursor-pointer"
+      onClick={() => set((state) => !state)}
+    >
+      <a.img
+        className="w-full rounded-sm sm:w-3/4 row-start-1 col-start-1 sm:mx-auto cursor-pointer hover:shadow-lg"
+        src={`/assets/fighters/${faction}/${faction}-${index}.png`}
+        style={{
+          opacity: opacity.to((o) => 1 - o),
+          transform,
+        }}
+      />
+      <a.img
+        className="w-full rounded-sm sm:w-3/4 row-start-1 col-start-1 sm:mx-auto cursor-pointer hover:shadow-lg"
+        src={`/assets/fighters/${faction}/${faction}-${index}-inspired.png`}
+        style={{
+          opacity,
+          transform: transform.to((t) => `${t} rotateY(180deg)`),
+        }}
+      />
+    </div>
+  );
 }
