07edf46addb74d5018dc73f504e2037da44d72f7
Oleh Lutsenko <pompolutz@gmail.com>
2024-04-27 09:40:58 +0000

    add carousel to see all plot cards

--- apps/frontend/src/pages/Deck/ReadonlyDeck/atoms/DeckPlotCards.jsx
+++ apps/frontend/src/pages/Deck/ReadonlyDeck/atoms/DeckPlotCards.jsx
@@ -1,65 +1,102 @@
-import React from "react";
-import { warbandHasPlot, getFactionByName, setHasPlot, plots } from "@wudb";
-import { useState } from "react";
-import { ModalPresenter } from "../../../../main";
+import { getFactionByName, plots, setHasPlot, warbandHasPlot } from "@wudb";
+import { usePortal } from "../../../../hooks/usePortal";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { forwardRef, useEffect, useState } from "react";
 
 const checkDeckHasPlots = (faction, sets) => {
-    return (
-        warbandHasPlot(getFactionByName(faction).id) ||
-        sets.some((setId) => setHasPlot(setId))
-    );
+  return (
+    warbandHasPlot(getFactionByName(faction).id) ||
+    sets.some((setId) => setHasPlot(setId))
+  );
 };
 
 const getPlotKeywords = (faction, sets) => {
-    if (!checkDeckHasPlots(faction, sets)) return [];
+  if (!checkDeckHasPlots(faction, sets)) return [];
 
-    const plotInfos = Object.values(plots);
+  const plotInfos = Object.values(plots);
 
-    return plotInfos.reduce((keywords, plot) => {
-        const factionWithPlot =
-            plot.connection === "Warband" && plot.name === faction;
-        const setWithPlot = plot.connection === "Set" && sets.includes(plot.id);
+  return plotInfos.reduce((keywords, plot) => {
+    const factionWithPlot =
+      plot.connection === "Warband" && plot.name === faction;
+    const setWithPlot = plot.connection === "Set" && sets.includes(plot.id);
 
-        return factionWithPlot || setWithPlot
-            ? [...keywords, plot]
-            : keywords;
-    }, []);
+    return factionWithPlot || setWithPlot ? [...keywords, plot] : keywords;
+  }, []);
 };
 
 export const DeckPlotCards = ({ factionId, sets }) => {
-    const [overlay, setOverlay] = useState(null);
+  const { Portal, open, portalClickAwayRef } = usePortal();
+  const plots = getPlotKeywords(factionId, sets);
 
-    return (
-        <div className="flex items-center space-x-1">
+  return (
+    <div className="flex items-center space-x-1">
+      <span>This deck includes plot cards:</span>
+      {plots.map(({ keyword }) => (
+        <span
+          className="font-bold underline hover:cursor-pointer"
+          key={keyword}
+          onClick={open}
+        >
+          {keyword}
+        </span>
+      ))}
 
-            <span>This deck includes plot cards:</span>
-            {getPlotKeywords(factionId, sets).map(({ keyword, asset }) => (
-                <span
-                    className="font-bold underline hover:cursor-pointer"
-                    key={keyword}
-                    onClick={() => setOverlay(asset)}
-                >
-                    {keyword}
-                </span>
-            ))}
-
-            {overlay && (
-                    <ModalPresenter>
-                        <div
-                            className="fixed inset-0 z-10 cursor-pointer"
-                            onClick={() => setOverlay(null)}
-                        >
-                            <div className="bg-black absolute inset-0 opacity-25"></div>
-                            <div className="absolute inset-0 z-20 flex justify-center items-center">
-                                <div className="w-4/5 lg:w-1/4">
-                                    <picture>
-                                        <img src={`/assets/plots/${overlay}.png`} />
-                                    </picture>
-                                </div>
-                            </div>
-                        </div>
-                    </ModalPresenter>
-                )}
+      <Portal>
+        <div className="grid w-full h-full place-content-center bg-purple-100/25">
+          <PlotsCarousel plots={plots} ref={portalClickAwayRef} />
         </div>
-    );
+      </Portal>
+    </div>
+  );
 };
+
+const PlotsCarousel = forwardRef(function PlotsCarousel({ plots = [] }, ref) {
+  const [api, setApi] = useState();
+  const [current, setCurrent] = useState(0);
+  const plotAssets = plots.flatMap((plot) =>
+    plot.asset ? [plot.asset] : plot.cards.map(({ asset }) => asset),
+  );
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  return (
+    <div className="flex flex-col items-center" ref={ref}>
+      <Carousel setApi={setApi} className="w-5/6 max-w-xs">
+        <CarouselContent>
+          {plotAssets.map((asset) => (
+            <CarouselItem key={asset}>
+              <picture>
+                <img
+                  src={`/assets/plots/${asset}.png`}
+                  className="rounded-xl"
+                />
+              </picture>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      <div className="py-2 text-center">
+        Card {current} of {plotAssets.length}
+      </div>
+    </div>
+  );
+});
--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "media",
+  darkMode: "class",
   theme: {
     extend: {
       inset: {
