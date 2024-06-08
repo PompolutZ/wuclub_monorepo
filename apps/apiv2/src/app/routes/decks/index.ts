2afa3c1354430b6d2a37e8c2cfababc9686e2594
Oleh Lutsenko <pompolutz@gmail.com>
2024-06-08 10:12:01 +0200

    return 404 if deck is not found by id

--- apps/apiv2/src/app/routes/decks/index.ts
+++ apps/apiv2/src/app/routes/decks/index.ts
@@ -27,6 +27,10 @@ export const getDeckById = async (req: ApiRequest, res: Response) => {
     const deckId = DeckIdSchema.parse(req.params.id);
     const deck = await _getDeckById(deckId);
 
+    if (!deck) {
+      return res.status(404).json({ error: "Deck not found" });
+    }
+
     if (deck.private && deck.fuid !== req.user?.fuid) {
       return res.status(403).json({ error: "Unauthorized" });
     }
