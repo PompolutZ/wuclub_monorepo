617fd31670264149fa5d5ff4fb8f8fd4771f33d4
Oleh Lutsenko <pompolutz@gmail.com>
2024-06-21 17:46:53 +0200

    format file

--- apps/frontend/src/pages/Home/index.jsx
+++ apps/frontend/src/pages/Home/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useHistory } from "react-router-dom";
 import DeckMetaSummary from "../../molecules/DecksMetaSummary";
 import Footer from "../../components/Footer";
@@ -7,40 +6,41 @@ import { grouppedFactions } from "../../data/wudb";
 import { AutosuggestSearch } from "./Search";
 
 const Home = () => {
-    const history = useHistory();
-    const { result: stats } = useFetch("api/v1/public-decks/stats");
+  const history = useHistory();
+  const { result: stats } = useFetch("api/v1/public-decks/stats");
 
-    const handleGlobalSearchClick = (payload) => {
-        history.push(`/view/card/${payload.id}`);
-    };
+  const handleGlobalSearchClick = (payload) => {
+    history.push(`/view/card/${payload.id}`);
+  };
 
-    return (
-        <div className="flex flex-col relative">
-            <h1 className="block text-2xl my-16 px-2 text-center text-white font-semibold">
-                Deck building website for Warhammer Underworlds.
-            </h1>
+  return (
+    <div className="flex flex-col relative">
+      <h1 className="block text-2xl my-16 px-2 text-center text-white font-semibold">
+        Deck building website for Warhammer Underworlds.
+      </h1>
 
-            <div className="mb-16 justify-center">
-                <div className="mx-4 sm:mx-auto sm:w-1/2 lg:w-1/3">
-                    <AutosuggestSearch onClick={handleGlobalSearchClick} />
-                </div>
-            </div>
-
-            <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
-                {
-                    grouppedFactions().slice(1).flatMap(({ factions }) => factions).map((faction) => (
-                        <DeckMetaSummary
-                            key={faction.abbr}
-                            faction={faction.name}
-                            stats={stats}
-                        />
-                    ))
-                }
-            </div>
-
-            <Footer />
+      <div className="mb-16 justify-center">
+        <div className="mx-4 sm:mx-auto sm:w-1/2 lg:w-1/3">
+          <AutosuggestSearch onClick={handleGlobalSearchClick} />
         </div>
-    );
+      </div>
+
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        {grouppedFactions()
+          .slice(1)
+          .flatMap(({ factions }) => factions)
+          .map((faction) => (
+            <DeckMetaSummary
+              key={faction.abbr}
+              faction={faction.name}
+              stats={stats}
+            />
+          ))}
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
