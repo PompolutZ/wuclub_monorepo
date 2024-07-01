492e40512a0b0dfe6cab6c5db1d3e31240e84250
Oleh Lutsenko <pompolutz@gmail.com>
2024-07-01 19:40:39 +0200

    add stats endpoint

--- apps/apiv2/src/app/index.ts
+++ apps/apiv2/src/app/index.ts
@@ -1,11 +1,13 @@
 import { app as decks } from "@/app/routes/decks";
 import { app as users } from "@/app/routes/users";
+import { app as stats } from "@/app/routes/stats";
 import { Hono } from "hono";
 
 export const app = new Hono()
   .basePath("/v2")
   .route("/users", users)
-  .route("/decks", decks);
+  .route("/decks", decks)
+  .route("/stats", stats);
 
 export type AppRoutes = typeof app;
 
---
+++ apps/apiv2/src/app/routes/stats.ts
@@ -0,0 +1,11 @@
+import { Hono } from "hono";
+import { getAllDeckStats } from "../../dal/stats";
+
+export const app = new Hono().get("/decks", async (c) => {
+  try {
+    const stats = await getAllDeckStats();
+    return c.json({ data: stats });
+  } catch (e) {
+    return c.json({ status: 500, error: "Internal server error" });
+  }
+});
