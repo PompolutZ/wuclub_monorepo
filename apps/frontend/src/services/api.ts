799b7f5ef7e6db028a1d93caef986a2d46e94abd
Oleh Lutsenko <pompolutz@gmail.com>
2024-07-12 11:01:41 +0200

    change to env variable

--- apps/frontend/src/services/api.ts
+++ apps/frontend/src/services/api.ts
@@ -1,4 +1,4 @@
 import { hc } from "hono/client";
 import type { AppRoutes } from "./app";
 
-export const api = hc<AppRoutes>("http://localhost:8181");
+export const api = hc<AppRoutes>(import.meta.env.VITE_API_BASE_URL);
