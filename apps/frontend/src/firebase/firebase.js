7e7920b5032eaf5b96f8543e896129b6986ea36f
Oleh Lutsenko <pompolutz@gmail.com>
2024-07-18 15:20:23 +0200

    remove axios and apiv1

--- apps/frontend/src/firebase/firebase.js
+++ apps/frontend/src/firebase/firebase.js
@@ -11,11 +11,8 @@ import {
   sendPasswordResetEmail,
   signInWithRedirect,
 } from "firebase/auth";
-// import "firebase/auth";
-// import "firebase/analytics";
-import axios from "axios";
+import { api } from "@/services/api";
 
-// import { prodConfig, devConfig } from './config';
 const config = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -26,17 +23,6 @@ const config = {
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
-
-// const prodConfig = {
-//     apiKey: import.meta.env.VITE_PROD_API_KEY,
-//     authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
-//     databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
-//     projectId: process.env.REACT_APP_PROD_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
-// }
-
-// const config = process.env.REACT_APP_ENVIRONMENT === 'prod' ? prodConfig : devConfig
 
 const Firebase2 = (function () {
   const app = initializeApp(config);
@@ -90,18 +76,17 @@ const Firebase2 = (function () {
       return onAuthStateChanged(auth, async (user) => {
         if (user) {
           const token = await user.getIdToken();
-          const userInfo = await axios.get(
-            `${import.meta.env.VITE_WUNDERWORLDS_API_ORIGIN}/api/v1/users`,
-            {
-              headers: {
-                authtoken: token,
-              },
+          const response = await api.v2.users.$get(undefined, {
+            headers: {
+              authtoken: token,
             },
-          );
+          });
+          const userInfo = await response.json();
+          console.log("userInfo", userInfo);
 
-          if (userInfo.data) {
+          if (userInfo) {
             next({
-              ...userInfo.data,
+              ...userInfo,
               uid: user.uid,
               isNew: false,
             });
