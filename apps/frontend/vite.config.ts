1e64487aabbc2b0eb8a55395738d949874ec2f88
Oleh Lutsenko <pompolutz@gmail.com>
2024-07-18 15:06:36 +0200

    remove comment

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import path from "path";
-// const reactSvgPlugin = require('vite-plugin-react-svg');
 
 // https://vitejs.dev/config/
 export default defineConfig({
