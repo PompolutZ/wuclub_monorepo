c537fb472015b52d1de31a97c2b82617ac2acfff
Oleh Lutsenko <pompolutz@gmail.com>
2024-07-19 17:13:28 +0200

    change home page implementation

--- apps/apiv2/src/app/routes/stats.ts
+++ apps/apiv2/src/app/routes/stats.ts
@@ -5,7 +5,7 @@ import { HTTPException } from "hono/http-exception";
 export const app = new Hono().get("/decks", async (c) => {
   try {
     const stats = await getAllDeckStats();
-    return c.json(stats);
+    return c.json(stats ?? []);
   } catch (e) {
     console.error("Error in getAllDeckStats:", e);
     throw new HTTPException(500, { message: "Internal server error" });
--- apps/apiv2/src/dal/stats.ts
+++ apps/apiv2/src/dal/stats.ts
@@ -1,15 +1,18 @@
+import { DeckStat } from "@fxdxpz/schema";
 import { getOrCreateClient } from "./client";
 
 export const getAllDeckStats = async () => {
   try {
     const client = await getOrCreateClient();
-    return await client
+    return (await client
       .collection("decks")
       .aggregate([
         { $match: { private: false } },
         { $group: { _id: "$faction", count: { $sum: 1 } } },
+        { $sort: { count: -1 } },
+        { $project: { _id: 0, faction: "$_id", count: 1 } },
       ])
-      .toArray();
+      .toArray()) as DeckStat[];
   } catch (e) {
     console.error("getAllDeckStats error: ", e);
   }
