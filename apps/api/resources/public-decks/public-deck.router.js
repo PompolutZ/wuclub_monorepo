a3239dad8f7f80645f2931d7c496c80906f2a305
Oleh Lutsenko <pompolutz@gmail.com>
2024-07-21 12:56:52 +0200

    delete apiv1 from monorepo

--- apps/api/resources/public-decks/public-deck.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllPublicDecks, getPublicDecksStats } from "./public-deck.controller.js";
-
-const router = Router();
-
-router.post('/', getAllPublicDecks);
-router.get('/stats', getPublicDecksStats);
-
-export default router;
