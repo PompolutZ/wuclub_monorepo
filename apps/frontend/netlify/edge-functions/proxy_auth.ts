38448005f01e77769d5d607282b1bdee1fe38ebb
Oleh Lutsenko <pompolutz@gmail.com>
2024-07-22 17:49:08 +0200

    update logs for edge function

--- apps/frontend/netlify/edge-functions/proxy_auth.ts
+++ apps/frontend/netlify/edge-functions/proxy_auth.ts
@@ -1,13 +1,21 @@
 export default async (request, context) => {
   const url = new URL(request.url);
   if (url.pathname.startsWith("/__/auth/")) {
-    console.log("Incoming request", JSON.stringify(request, null, 2));
+    console.log("Incoming request", request);
+    console.log(request.url);
+    console.log(request.headers);
+    console.log(request.method);
+    console.log(request.body);
 
     const newUrl = new URL(request.url);
     newUrl.hostname = "yawudb.firebaseapp.com";
 
     const proxyRequest = new Request(newUrl.toString(), request);
-    console.log("Proxy request", JSON.stringify(proxyRequest, null, 2));
+    console.log("Proxy request", proxyRequest);
+    console.log(proxyRequest.url);
+    console.log(proxyRequest.headers);
+    console.log(proxyRequest.method);
+    console.log(proxyRequest.body);
 
     return fetch(proxyRequest);
   }
