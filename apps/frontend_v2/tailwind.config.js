47415d622eeff1e64a19e1af1c31ca8bbcdec109
Oleh Lutsenko <pompolutz@gmail.com>
2024-11-01 11:49:59 +0100

    add shadcn

--- apps/frontend_v2/tailwind.config.js
+++ apps/frontend_v2/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports, no-undef
+  plugins: [require("tailwindcss-animate")],
 };
