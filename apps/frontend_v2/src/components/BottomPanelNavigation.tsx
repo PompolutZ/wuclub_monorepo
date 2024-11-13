c856655e7a070997806069fdda477207154506f4
Oleh Lutsenko <pompolutz@gmail.com>
2024-11-13 21:57:21 +0100

    start moving deck builder

---
+++ apps/frontend_v2/src/components/BottomPanelNavigation.tsx
@@ -0,0 +1,42 @@
+export type Tab = {
+  name: string;
+  Icon: React.ComponentType<{ className?: string }>;
+};
+
+export type BottomPanelNavigationProps = {
+  activeTabIndex: number;
+  setActiveTabIndex: (activeIndex: number) => void;
+  tabs: Tab[];
+};
+
+export function BottomPanelNavigation({
+  activeTabIndex,
+  setActiveTabIndex,
+  tabs,
+}: BottomPanelNavigationProps) {
+  return (
+    <div
+      className={`flex transition-colors duration-500 bg-gradient-to-r ${
+        activeTabIndex == 0
+          ? "from-purple-200 via-gray-100 to-gray-100"
+          : activeTabIndex == 1
+            ? "from-gray-100 via-purple-200 to-gray-100"
+            : "from-gray-100 via-gray-100 to-purple-200"
+      }`}
+    >
+      {tabs.map(({ name, Icon }: Tab, index: number) => (
+        <button
+          key={name}
+          className={`flex-1 flex flex-col items-center py-2 text-xs
+                ${
+                  activeTabIndex === index ? "text-purple-700" : "text-gray-700"
+                }`}
+          onClick={() => setActiveTabIndex(index)}
+        >
+          <Icon className="h-6 fill-current mb-1" />
+          {name}
+        </button>
+      ))}
+    </div>
+  );
+}
---
+++ apps/frontend_v2/src/hooks/useResizeHeight.ts
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import useMeasure from "react-use-measure";
+
+export function useResizeHeight(config: { open: boolean }) {
+    const [open, setOpen] = useState(config.open);
+    const [ref, { height }] = useMeasure();
+
+    const toggle = () => setOpen((prev) => !prev);
+
+    return [ref, open, toggle, height];
+}
