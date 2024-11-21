493ccdf76fec09ef791244c074f060c0e37ae957
Oleh Lutsenko <pompolutz@gmail.com>
2024-11-21 20:19:40 +0100

    add netlify config

--- apps/astro-club/astro.config.mjs
+++ apps/astro-club/astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
+});
