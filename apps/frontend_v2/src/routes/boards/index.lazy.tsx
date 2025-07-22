1542ab3cb4a00e653ca11edeab2c92974f478587
Oleh Lutsenko <pompolutz@gmail.com>
2025-07-22 14:16:44 +0200

    delete v2 attempt

--- apps/frontend_v2/src/routes/boards/index.lazy.tsx
+++
@@ -1,5 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
-
-export const Route = createLazyFileRoute('/boards/')({
-  component: () => <div>Hello /boards/!</div>,
-})
--- apps/frontend_v2/src/routes/decks/index.lazy.tsx
+++
@@ -1,5 +0,0 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-export const Route = createLazyFileRoute("/decks/")({
-  component: () => <div>Hello /decks/!</div>,
-});
