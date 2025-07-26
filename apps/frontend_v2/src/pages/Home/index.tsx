1e55dc6c06ef7cf280e1286179563d182dff3ea0
Oleh Lutsenko <pompolutz@gmail.com>
2025-07-26 12:01:36 +0200

    try 3d dice roller

--- apps/frontend_v2/src/pages/Home/index.tsx
+++ apps/frontend_v2/src/pages/Home/index.tsx
@@ -3,13 +3,66 @@ import Footer from "../../components/Footer";
 import { Card } from "../../data/wudb/types";
 // import DeckMetaSummary from "./DecksMetaSummary";
 import { AutosuggestSearch } from "./Search";
+import DisplayResults from "@3d-dice/dice-ui/src/displayResults"; // fui index exports are messed up -> going to src
+import DiceParser from "@3d-dice/dice-parser-interface";
+import { Dice } from "../../shared/components/DiceBox";
 // import { useDeckStats } from "./useDeckStats";
+
+// create Dice Roll Parser to handle complex notations
+const DRP = new DiceParser();
+
+// create display overlay for final results
+const DiceResults = new DisplayResults("#dice-box");
+
+// initialize the Dice Box outside of the component
+Dice.init().then(() => {
+  // clear dice on click anywhere on the screen
+  document.addEventListener("mousedown", () => {
+    const diceBoxCanvas = document.getElementById("dice-canvas");
+    if (window.getComputedStyle(diceBoxCanvas).display !== "none") {
+      Dice.hide().clear();
+      DiceResults.clear();
+    }
+  });
+});
+
+const AdvRollBtn = (props) => {
+  const { label, onRoll, notation } = props;
+
+  const roll = () => {
+    onRoll(notation);
+  };
+
+  return <button onClick={roll}>{label}</button>;
+};
 
 const Home = () => {
   const history = useHistory();
   // const { data: stats } = useDeckStats();
   const handleGlobalSearchClick = (payload: Card) => {
     history.push(`/view/card/${payload.id}`);
+  };
+
+  Dice.onRollComplete = (results) => {
+    console.log(results);
+
+    // handle any rerolls
+    const rerolls = DRP.handleRerolls(results);
+    if (rerolls.length) {
+      rerolls.forEach((roll) => Dice.add(roll, roll.groupId));
+      return rerolls;
+    }
+    // if no rerolls needed then parse the final results
+    const finalResults = DRP.parseFinalResults(results);
+
+    // show the results
+    DiceResults.showResults(finalResults);
+  };
+
+  // trigger dice roll
+  const rollDice = (notation, group) => {
+    // trigger the dice roll using the parser
+    Dice.show().roll(DRP.parseNotation(notation));
   };
 
   return (
@@ -23,6 +76,8 @@ const Home = () => {
           <AutosuggestSearch onClick={handleGlobalSearchClick} />
         </div>
       </div>
+
+      <AdvRollBtn label="d20 Advantage" notation="2d20kh1" onRoll={rollDice} />
 
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 min-h-screen">
         {/* {stats?.map(({ faction, count }) => (
